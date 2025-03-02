a0f568ffef90cbe0e22de9789554d4acc43cd874
Aayushi Anand <aayushianand@fico.com>
2025-03-02 19:34:03 +0530

    [March 2, 2025]basic maths completed

---
+++ TUF_SDE_SHEET/STEP1/LECTURE4/CheckPalindromeInIntegers.java
@@ -0,0 +1,30 @@
+package TUF_SDE_SHEET.STEP1.LECTURE4;
+
+public class CheckPalindromeInIntegers {
+
+    // We don't need to check the boundary condition because if x is in the range,
+    // it's palindrome will also be x. So, it will automatically be in the range of integers.
+    public static int getReverse(int x) {
+        int reversed = 0;
+
+        while(x != 0) {
+            int last_digit = x%10;
+            reversed = reversed * 10 + last_digit;
+            x = x/10;
+        }
+
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        return x == getReverse(x);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1001));
+    }
+}
--- TUF_SDE_SHEET/STEP1/LECTURE4/ReverseIntegers.java
+++ TUF_SDE_SHEET/STEP1/LECTURE4/ReverseIntegers.java
@@ -2,4 +2,29 @@ package TUF_SDE_SHEET.STEP1.LECTURE4;
 
 public class ReverseIntegers {
 
+    public static int reverse(int x) {
+        if(x == 0 || x == Integer.MAX_VALUE) {
+            return 0;
+        }
+
+        int reversed_num = 0;
+        while(x != 0) {
+            int last_digit = x%10;
+            if(reversed_num > (Integer.MAX_VALUE - Math.abs(last_digit))/10) {
+                return 0;
+            } else if(reversed_num < (Integer.MIN_VALUE + Math.abs(last_digit))/10) {
+                return 0;
+            }
+            reversed_num = reversed_num*10 + last_digit;
+            x=x/10;
+        }
+
+        return reversed_num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(1534236469));
+        System.out.println(reverse(-123));
+    }
+
 }
