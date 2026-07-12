68c8b33d46e6936d1be83c202d0a331fce684644
Aayushi1001 <aayushianand08@gmail.com>
2026-07-12 19:33:50 +0530

    Binary tree hard problems

--- DataStructure/BinaryTree/Hard/ChildrenSum.java
+++ DataStructure/BinaryTree/Hard/ChildrenSum.java
@@ -1,4 +1,52 @@
 package DataStructure.BinaryTree.Hard;
 
+import DataStructure.BinaryTree.IterativeLevelOrder;
+import DataStructure.BinaryTree.utils.TreeNode;
+
 public class ChildrenSum {
+
+    public static void childrenSum(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        int childSum = 0;
+        if(root.left != null) {
+            childSum += root.left.val;
+        }
+
+        if(root.right != null) {
+            childSum += root.right.val;
+        }
+
+        if(childSum < root.val) {
+            if(root.left != null) {
+                root.left.val = root.val;;
+            } else if(root.right != null) {
+                root.right.val = root.val;
+            }
+        }
+
+        childrenSum(root.left);
+        childrenSum(root.right);
+
+        int sum = 0;
+        if(root.left != null) {
+            sum += root.left.val;
+        }
+
+        if(root.right != null) {
+            sum += root.right.val;
+        }
+
+        if(sum > root.val) {
+            root.val = sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.createTree();
+        childrenSum(root);
+        IterativeLevelOrder.levelOrder(root);
+    }
 }
--- DataStructure/BinaryTree/Hard/MaximumWidth.java
+++ DataStructure/BinaryTree/Hard/MaximumWidth.java
@@ -1,4 +1,55 @@
 package DataStructure.BinaryTree.Hard;
 
+import DataStructure.BinaryTree.utils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumWidth {
+    static class NodePosition {
+        TreeNode node;
+        int position;
+
+        NodePosition(TreeNode node, int position) {
+            this.node = node;
+            this.position = position;
+        }
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<NodePosition> queue = new LinkedList<>();
+        queue.offer(new NodePosition(root, 0));
+        int maxWidth = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int leftmost = 0, rightmost = 0;
+
+            for (int i = 0; i < size; i++) {
+                NodePosition current = queue.poll();
+                TreeNode node = current.node;
+                int pos = current.position;
+
+                if (i == 0) leftmost = pos;
+                if (i == size - 1) rightmost = pos;
+
+                if (node.left != null) {
+                    queue.offer(new NodePosition(node.left, pos * 2));
+                }
+                if (node.right != null) {
+                    queue.offer(new NodePosition(node.right, pos * 2 + 1));
+                }
+            }
+
+            maxWidth = Math.max(maxWidth, rightmost - leftmost + 1);
+        }
+
+        return maxWidth;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
